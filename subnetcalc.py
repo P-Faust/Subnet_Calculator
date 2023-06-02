c194333182a2fd691bbd27834957ebb50a82c657
P-Faust <patrik.schmidt97@yahoo.de>
2023-06-02 09:34:15 +0000

    Kommentare

--- subnetcalc.py
+++ subnetcalc.py
@@ -2,6 +2,7 @@ import argparse
 from netaddr import *
 from prettytable import PrettyTable
 
+# Gibt die Subnetz-Bits im Binärformat zurück
 def get_subnet_bin(args):
     if(args.prefix):
         subnet = IPAddress("0.0.0.0")
@@ -13,6 +14,7 @@ def get_subnet_bin(args):
         subnet_binary = subnet.bin[2:]
         return subnet_binary
     
+# Gibt eine Liste von Binärzahlen zurück, die die benötigten Bits für die Subnetze darstellen
 def get_needed_bits(args):
     needed_bits = get_subnet_bit_count(args)
     bit_list = []
@@ -22,6 +24,7 @@ def get_needed_bits(args):
         count += 1    
     return bit_list
 
+# Gibt die Anzahl der benötigten Bits für die Subnetze zurück
 def get_subnet_bit_count(args):
     needed_bits = 0
     pow_sum = 0
@@ -30,6 +33,7 @@ def get_subnet_bit_count(args):
         pow_sum = pow(2,needed_bits)
     return needed_bits
 
+# Gibt den Index des letzten Bits zurück, das nicht "1" ist
 def get_last_bit(subnet_list):
     count = 0
     for x in subnet_list:
@@ -39,6 +43,7 @@ def get_last_bit(subnet_list):
             count += 1
     return count
 
+# Druckt die Subnetze, Netzwerkinformationen und Broadcast-Adressen
 def print_subnets(subnetmask, last_bit, subnetbits, ip, args):
     count = 0
     while count < len(subnetbits):
@@ -48,6 +53,7 @@ def print_subnets(subnetmask, last_bit, subnetbits, ip, args):
         count += 1
     print(table)
 
+# Druckt Host-Informationen für ein Subnetz
 def print_hosts(subnetmask,last_bit,ip,args, subnet_id):
     ip_bin = ip.bin[2:]
     ip_cut = ip_bin[:last_bit]
@@ -62,6 +68,7 @@ def print_hosts(subnetmask,last_bit,ip,args, subnet_id):
     broadcast = binary_ip_to_decimal(broadcast_ip,False,False)
     table.add_row([subnet_id, net_id, first_host, last_host, broadcast])
 
+# Gibt die Broadcast-Adresse für ein Subnetz zurück
 def get_broadcast(ip,last_bit,needed_bit):
     last_host = ip[:]
     last_host = last_host[last_bit+needed_bit::1]
@@ -72,6 +79,7 @@ def get_broadcast(ip,last_bit,needed_bit):
     broadcast_ip = ''.join(broadcast_ip)
     return broadcast_ip
 
+# Konvertiert eine Binär-IP-Adresse in eine Dezimal-IP-Adresse
 def binary_ip_to_decimal(ip_str, bool_first_host, bool_last_host):
     octet_one = ip_str[0:8]
     octet_one = int(octet_one, 2)
@@ -89,6 +97,7 @@ def binary_ip_to_decimal(ip_str, bool_first_host, bool_last_host):
         dec_ip = f"{octet_one}.{octet_two}.{octet_three}.{octet_four-1}"
     return dec_ip
 
+# Berechnet die neue Subnetzmaske für das letzte Subnetz und gibt sie zurück
 def get_new_subnet_mask(subnetmask,last_bit, needed_bits, subnet_bits):
     new_subnet_mask = subnetmask[:]
     new_subnet_mask[last_bit:last_bit+needed_bits] = subnet_bits[-1]
@@ -96,6 +105,7 @@ def get_new_subnet_mask(subnetmask,last_bit, needed_bits, subnet_bits):
     new_subnet_mask = binary_ip_to_decimal(new_subnet_mask, False, False)
     return new_subnet_mask
     
+# Berechnet den neuen CIDR-Präfix für das letzte Subnetz und gibt ihn zurück
 def get_new_prefix(new_subnet_mask):
     prefix = 0
     subnet_mask = IPAddress(new_subnet_mask)
@@ -105,6 +115,7 @@ def get_new_prefix(new_subnet_mask):
             prefix += 1 
     return prefix
 
+# Überprüft, ob das Subnetz gültig ist (nicht zu viele Hosts)
 def validate_subnet(last_bit, needed_bit):
     valid = False
     if (last_bit + needed_bit < 31):
@@ -113,6 +124,7 @@ def validate_subnet(last_bit, needed_bit):
         valid = False
     return valid
 
+# Hauptfunktion, die den Code ausführt
 def main():
     global table
     table = PrettyTable()
